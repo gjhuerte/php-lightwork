ff83317bfff87604b0d0b60f658fc7997ee36c7e
Gabriel Jay Huerte <gjhuerte@gmail.com>
2019-01-13 13:05:40 +0800

    Create a redirection module for the application

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php include('./public/index.php') ?>
+<?php 
+
+/** 
+ *  |---------------------------------------------------------------------------------|
+ *  | Redirection module                                                              |
+ *  | --------------------------------------------------------------------------------|
+ *  |                                                                                 |
+ *  | Calls the index.php file under the public folder which is the original callable |
+ *  | This module is just for creating a different landing page for the website which |
+ *  | separates the main landing page from the callable landing pages                 |
+ *  |                                                                                 |
+ *  |---------------------------------------------------------------------------------|
+*/
+include('./public/index.php');
