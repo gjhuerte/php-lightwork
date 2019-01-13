cf512c836186cb72d91cad40e58694545f9d3970
Gabriel Jay Huerte <gjhuerte@gmail.com>
2019-01-13 13:08:28 +0800

    Remake the comment on index page

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 
-/** 
- *  |---------------------------------------------------------------------------------|
- *  | Redirection module                                                              |
- *  | --------------------------------------------------------------------------------|
- *  |                                                                                 |
- *  | Calls the index.php file under the public folder which is the original callable |
- *  | This module is just for creating a different landing page for the website which |
- *  | separates the main landing page from the callable landing pages                 |
- *  |                                                                                 |
- *  |---------------------------------------------------------------------------------|
+/*
+|---------------------------------------------------------------------------------|
+| Redirection module                                                              |
+| --------------------------------------------------------------------------------|
+|                                                                                 |
+| Calls the index.php file under the public folder which is the original callable |
+| This module is just for creating a different landing page for the website which |
+| separates the main landing page from the callable landing pages                 |
+|                                                                                 |
+|---------------------------------------------------------------------------------|
 */
 include('./public/index.php');
